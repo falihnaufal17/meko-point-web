e36fa6039cac2cbbb07746880b8168d67d6c34b0
Falih <35053882+falihnaufal17@users.noreply.github.com>
2025-06-29 22:04:55 +0700

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '../globals.css'
-import { translations, Language } from '../lib/translations'
+import './globals.css'
+import { translations, Language } from './lib/translations'
 
 const inter = Inter({ subsets: ['latin'] })
 
