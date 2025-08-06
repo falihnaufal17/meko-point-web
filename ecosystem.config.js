73d142b98d526c4b33fae7dd09d8d7c48c02a5f0
root <root@personal-server.neo.internal>
2025-08-06 22:38:59 +0700

    test cicd

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'meko-point-web',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
+
