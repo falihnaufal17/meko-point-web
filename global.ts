9011c51adc9cddf30b5811fe98b98ec683918e57
Falih Naufal <falihnaufal1700@gmail.com>
2025-09-27 20:23:17 +0700

    fix: lint

--- global.ts
+++ global.ts
@@ -1,5 +1,5 @@
 import {routing} from '@/i18n/routing';
-import {formats} from '@/i18n/request';
+import formats from '@/i18n/request';
 import messages from './locales/id.json';
  
 declare module 'next-intl' {
